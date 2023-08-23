0403bce592a33edcf6288cc55a33bf8d4b8ac9c9
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-23 16:25:56 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Les Controllers en Symfony</title>
+  </head>
+  <body>
+<h1>Création du Contrôleur</h1>
+    <p>
+      Il existe plusieurs façcons de créer un controleur/ en créant un fichier PHP dédié à une classe controleur ou en utilisant les lignes de commandes.
+
+      Ainsi, si l'on souhaite créer une classe controleur "à la main " dans un fichier PH¨P, il conviendra de la placer dans le dossier "src" puis dans un dossier "contro'ller".
+
+      Il convient également de créer une classe controller par fichier , et celui-ci portera le même nom que la classe. A l'interieur de la classe , nous pouvons utiliser autant de méthode que vous le souhaitons.
+
+      ==> Conseil: 
+      utilisez "Codacy", " SymfonyInseight ", "CodeClimate" pour auditer la qualité de notre code. En effet, ils permettent de détécter une fonction surchargée de notre code.
+    </p>
+  
+  </body>
+</html>
